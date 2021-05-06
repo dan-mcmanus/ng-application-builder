adf228dd1731dfe210fc8786483933eb272e2d89
Dan McManus <dan.mcmanus@mrsreps.com>
2021-05-06 12:31:00 -0500

    upgrade from ng10 to ng11

--- ng-app-builder-web/src/app/dashboard/new-dialog/new-dialog.component.spec.ts
+++ ng-app-builder-web/src/app/dashboard/new-dialog/new-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NewDialogComponent } from './new-dialog.component';
 
@@ -6,7 +6,7 @@ describe('NewDialogComponent', () => {
   let component: NewDialogComponent;
   let fixture: ComponentFixture<NewDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewDialogComponent ]
     })
